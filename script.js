e3cda12e5160b4422d086759cc7aa7e64afb46a9
Matthew Hope <matt-hope@hotmail.com>
2020-11-05 12:20:31 -0500

    added pseudocode coments to pig latin function

--- script.js
+++ script.js
@@ -2,10 +2,26 @@
 const vowels = ["a", "e", "i", "o", "u"];
 
 function translatePigLatin(str) {
-    // use spread str into an array
-    [...str].forEach(letter => {
-      console.log(letter);
-    });
+  // use spread str into an array
+
+  // get first letter
+
+  // check if first letter is consonant
+
+  // check for consonant cluster
+
+  // if there's a consonant cluster
+  // find out where it ends to get all letters in cluster
+
+  // move the letter or cluster to the end of the word
+
+  // add ay to the end of the word
+
+
+  // if first letter is vowel
+
+  // add way to the end of the word
+
   return str;
 }
 

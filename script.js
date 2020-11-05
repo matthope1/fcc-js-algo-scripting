9711acb4bacae12e19f24c9bbed46b4e9dafdd4b
Matthew Hope <matt@Matthews-MacBook-Air.local>
2020-11-05 11:48:51 -0500

    added loop to pig latin function

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 
-const consonants = ["a", "e", "i", "o", "u"];
+const vowels = ["a", "e", "i", "o", "u"];
 
 function translatePigLatin(str) {
+    // use spread str into an array
+    [...str].forEach(letter => {
+      console.log(letter);
+    });
   return str;
 }
 

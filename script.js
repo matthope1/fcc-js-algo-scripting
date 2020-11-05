4c104c1cc6e5f3c24e413bc1e6fb9337331cd2f0
Matthew Hope <matt-hope@hotmail.com>
2020-11-05 16:03:52 -0500

    started adding functionality to find consonant cluster

--- script.js
+++ script.js
@@ -6,23 +6,36 @@ function translatePigLatin(str) {
 
   // get first letter
 
+  const first_letter = str[0];
+  const strLen = str.length;
+  
   // check if first letter is consonant
+  if (!vowels.includes(first_letter)) {
+    // check for consonant cluster
 
-  // check for consonant cluster
+    // if there's a consonant cluster
+    // find out where it ends to get all letters in cluster
+    let i = 0;
+    for (i; i < strLen; i++) {
+      // find index of first item that is a vowel
+      if (vowels.includes(str[i])) {
+          break; 
+      }
+    }
+    
+    let cluster = str.split(0,i);
+    let endOfString = str.split(i,strLen);
 
-  // if there's a consonant cluster
-  // find out where it ends to get all letters in cluster
+    // move the letter or cluster to the end of the word
 
-  // move the letter or cluster to the end of the word
-
-  // add ay to the end of the word
-
-
-  // if first letter is vowel
-
-  // add way to the end of the word
-
+    // add ay to the end of the word
+  }
+  else {
+    // if first letter is vowel
+    // add way to the end of the word
+  }
+  
   return str;
 }
 
-translatePigLatin("consonant");
+translatePigLatin("glove");
